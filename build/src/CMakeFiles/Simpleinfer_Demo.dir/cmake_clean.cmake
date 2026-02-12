1e61628817ccebf5a288205eba9f827c397e0c72
ethanZZZZZZZZZZZZ <2499009810@qq.com>
2026-02-12 22:31:09 +0800

    feat:init project structure

---
+++ build/src/CMakeFiles/Simpleinfer_Demo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../bin/Simpleinfer_Demo"
+  "../bin/Simpleinfer_Demo.pdb"
+  "CMakeFiles/Simpleinfer_Demo.dir/main.cpp.o"
+  "CMakeFiles/Simpleinfer_Demo.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Simpleinfer_Demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, MyInfer!" << std::endl;
+    return 0;
+}
